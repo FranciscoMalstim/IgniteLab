3b010032845bd917380fcfdd2c7a32a7dd6bc9fa
FranciscoMalstim <franciscosantos2501@gmail.com>
2022-06-26 13:34:12 -0300

    Lesson 5 • Deploy, CodeGen

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
+import Footer from "../components/Footer";
 import { Header } from "../components/Header";
+import { Logo } from "../components/Logo";
 import { Sidebar } from "../components/Sidebar";
+import ImgUrl from "../../src/assets/code-mockup.png";
 import { Video } from "../components/Video";
+import { CaretCircleRight } from "phosphor-react";
 
 export function Event() {
 
@@ -13,7 +17,26 @@ export function Event() {
         <main className="flex flex-1">
           { slug 
             ? <Video lessonSlug={slug}/> 
-            : <div className="flex-1"></div>
+            : <div className="flex-1 bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+                <div className="flex mt-6 items-center">
+                  <div className=" mt-2 max-w-[640px]">
+                    <Logo />
+                    <h1 className="mt-8 text-[2.5rem] leading-tight">
+                      Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React JS</strong>     
+                    </h1>
+                    <div className="flex items-end">
+                      <h1 className="text-[2.5rem] leading-tight">
+                        Vamos <strong className="text-blue-500">começar</strong> ? <br/> Só Clicar em  <strong className="text-blue-500"> uma Aula ao lado</strong>  
+                      </h1>
+                      <CaretCircleRight size={32} className= "ml-3 mb-2" />
+                    </div>
+                  </div>
+                </div>
+                <img src={ImgUrl} className="mt-1" alt=""/>
+                <div className="w-full flex-1">
+                  <Footer />
+                </div>
+              </div>
           }
           <Sidebar/>
         </main>
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,17 +1,11 @@
-import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import { Logo } from "../components/Logo";
+import ImgUrl from "../../src/assets/code-mockup.png"
 import { ReactIcon } from "../components/ReactIcon";
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
-const CREATE_SUBSCRIBER_MUTATION = gql`
-  mutation CreateSubscriber ($name: String!, $email: String!) {
-  createSubscriber(data: {name: $name, email: $email}) {
-    id
-  }
-}
-` 
 
 export function Subscribe() {
   const navigate = useNavigate()
@@ -19,7 +13,7 @@ export function Subscribe() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const [createSubscribe, {loading}] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+  const [createSubscribe, {loading}] = useCreateSubscriberMutation()
 
   async function handleSubscribe(event: FormEvent) {
     event?.preventDefault();
@@ -50,40 +44,39 @@ export function Subscribe() {
             <p className="mt-4 text-gray-200 leading-relaxed">
               Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
             </p>
-        </div>
-
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-          <strong className="text-2xl mb-6  block"> Inscreva-se gratutamente</strong>
-        
-          <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
-            <input 
+        </div> 
+          <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+            <strong className="text-2xl mb-6  block"> Inscreva-se gratutamente</strong>
+          
+            <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+              <input 
+                className="bg-gray-900 rounded px-5 h-14"
+                type="text"
+                placeholder="Seu nome completo"
+                onChange={event => setName(event.target.value)}
+              />
+              <input 
               className="bg-gray-900 rounded px-5 h-14"
-              type="text"
-              placeholder="Seu nome completo"
-              onChange={event => setName(event.target.value)}
-            />
-            <input 
-            className="bg-gray-900 rounded px-5 h-14"
-            type="email"
-            placeholder="Digite seu e-mail"
-            onChange={event => setEmail(event.target.value)}
-            />
+              type="email"
+              placeholder="Digite seu e-mail"
+              onChange={event => setEmail(event.target.value)}
+              />
 
-            <button 
-              type="submit"
-              disabled={loading}
-              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50">
-              Garantir minha vaga
-            </button>
-          </form>
+              <button 
+                type="submit"
+                disabled={loading}
+                className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50">
+                Garantir minha vaga
+              </button>
+
+            </form>
+          </div>
         </div>
+        <img src={ImgUrl} className="mt-10" alt=""/>
       </div>
-      
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt=""/>
-    </div>
-    <div className="w-full">
-      <Footer />
-    </div>
+      <div className="w-full">
+        <Footer />
+      </div>
     </>
   )
 }
